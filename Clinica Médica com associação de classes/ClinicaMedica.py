411c6743bd1eb2009fe2240a8a32577142726ec0
Isaias Felipe <126331149+Isaiasfelipe7@users.noreply.github.com>
2023-05-03 20:53:51 -0300

    nova class

--- Clinica Médica com associação de classes/ClinicaMedica.py
+++ Clinica Médica com associação de classes/ClinicaMedica.py
@@ -1,7 +1,7 @@
 class Paciente:
     def __init__(self, id_pac, nom_pac, dt_nasc, contato):
         self.__id_paciente = id_pac
-        self.nome = nom_pac
+        self.nome_paciente = nom_pac
         self.__dt_nasc = dt_nasc
         self.__contato = contato
 
@@ -30,3 +30,7 @@ class ConsultaMedica:
         self.__data = data
         self.__pago = pago
         self.__data_retorno = True
+
+    def __str__(self):
+        return f'Consulta marcada para a data: {self.__data} \nPaciente: {self.__paciente.nome_paciente} \nMédico: {self.__medico.nome_medico}'
+
