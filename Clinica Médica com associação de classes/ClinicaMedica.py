1a1585770d00f7e9eec90b395ba1ca76398df5d0
Isaias Felipe <126331149+Isaiasfelipe7@users.noreply.github.com>
2023-05-04 10:41:59 -0300

    nova class

--- Clinica Médica com associação de classes/ClinicaMedica.py
+++ Clinica Médica com associação de classes/ClinicaMedica.py
@@ -1,4 +1,3 @@
-import MenuClinica
 from datetime import *
 
 class Paciente:
@@ -112,6 +111,15 @@ class ConsultaMedica:
             print(f'Retorno agendado para o dia: {data.strftime("%d/%m/%Y")}')
         else:
             print("Consulta não foi paga!")
+def menu():
+    print('\n   Clinica Médica    ')
+    print('\n1 - Cadastrar Paciente')
+    print('2 - Cadastrar Médico')
+    print('3 - Marcar Consulta')
+    print('4 - Pagar Consulta')
+    print('5 - Cancelar Consulta')
+    print('6 - Marcar Retorno')
+    print('0 - Sair')
 
 
 def main():
@@ -121,7 +129,7 @@ def main():
     medicos = [Medico(1, 1234, 'isaias', 'ortopedista')]
 
     while True:
-        MenuClinica.menu()
+        menu()
 
         op = input('\nEscolha uma opção: ')
         
--- Clinica Médica com associação de classes/MenuClinica.py
+++
@@ -1,9 +0,0 @@
-def menu():
-    print('\n   Clinica Médica    ')
-    print('\n1 - Cadastrar Paciente')
-    print('2 - Cadastrar Médico')
-    print('3 - Marcar Consulta')
-    print('4 - Pagar Consulta')
-    print('5 - Cancelar Consulta')
-    print('6 - Marcar Retorno')
-    print('0 - Sair')
