30c1686fc4582ec9be3209591c18ddb5353ac210
Isaias Felipe <126331149+Isaiasfelipe7@users.noreply.github.com>
2023-05-03 21:18:37 -0300

    novo

---
+++ Clinica Médica com associação de classes/MenuClinica.py
@@ -0,0 +1,9 @@
+def menu():
+
+    print('\n== ==== MENU ==== ==')
+    print('\n1 - Cadastrar Paciente')
+    print('2 - Cadastrar Médico')
+    print('3 - Marcar Consulta')
+    print('4 - Pagar Consulta')
+    print('5 - Cancelar Consulta')
+    print('6 - Marcar Retorno')
